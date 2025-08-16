7872ab7d611c0d94068be1518b4f8835e78de11c
ShenJing <shenjingnerve@gmail.com>
2025-08-16 20:36:32 +0800

    Deploy website - based on e6efdffc1f68218f967bd5d8ebb722f5771fc1e3

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/shenjing/Docusaurus_blog/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/shenjing/Docusaurus_blog/node_modules/infima/dist/css/default/default.css"),
-  require("/home/shenjing/Docusaurus_blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/shenjing/Docusaurus_blog/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/shenjing/Docusaurus_blog/src/css/custom.css"),
-];
